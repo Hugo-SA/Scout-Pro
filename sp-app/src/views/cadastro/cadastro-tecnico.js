0c21adf7a46bc71485b3a42a07a8d7865b480e3e
Vinicius <viniciuscguedesbsi@gmail.com>
2025-12-04 09:16:13 -0300

    adição do custom css para o tecnico

--- sp-app/src/views/cadastro/cadastro-tecnico.js
+++ sp-app/src/views/cadastro/cadastro-tecnico.js
@@ -8,7 +8,7 @@ import FormGroup from '../../components/form-group';
 
 import { mensagemSucesso, mensagemErro } from '../../components/toastr';
 
-//import '../custom.css';
+import '../../custom.css';
 
 import axios from 'axios';
 import { BASE_URL } from '../../config/axios';
